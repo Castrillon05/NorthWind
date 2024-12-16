363ebfa3c044fe0b477be1ffeb1dcd445dcde920
Danny castrillon <dannycastrillob2@hotmail.com>
2024-12-16 15:47:17 -0500

    Agregar archivos de proyecto.

---
+++ NorthWind.Entities/ValueObjects/UserAction.cs
@@ -0,0 +1,8 @@
+﻿namespace NorthWind.Entities.ValueObjects;
+public class UserAction(string user, string description)
+{
+    public DateTime CreatedDataTime { get; } = DateTime.Now;
+    public string User => User;
+    public string Description => Description;
+
+}
